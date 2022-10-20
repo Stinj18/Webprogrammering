c6b0fa76cdb6fa9332767b99a55968d60e9f3116
Stinj18 <98690299+Stinj18@users.noreply.github.com>
2022-10-20 13:57:14 +0200

    Update regristrationpage.php

--- Lektion 3/regristrationpage.php
+++ Lektion 3/regristrationpage.php
@@ -1,1 +1,18 @@
+<html>
+<head>
+        <title> Registration </title>
+        <script src="registrationscript.js"></script>
+</head>
+<body>
+    <form method="post" onsubmit="return chechform()">
+        <h2>Registre here</h2>
+        Create username: <input type = "text" name="username" id="username"/>
+        <br>
+        Create password: <input type = "text" name="password" id="password"/>
+        <br>
+        <input type="submit" name="registre">
+    </form>
 
+</body>
+
+</html>
