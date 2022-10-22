e2a49e8e316dc70149dddc74e3c94946e4c96c6d
MaleneMoehring <70900778+MaleneMoehring@users.noreply.github.com>
2022-10-22 12:22:05 +0200

    Update uploadpage.php

--- Lektion 3/uploadpage.php
+++ Lektion 3/uploadpage.php
@@ -1,7 +1,32 @@
-<?php 
-session_start(); /*creates a session or resumes the current one based on a session identifier passed via a GET or POST request, or passed via a cookie.*/
+<!DOCTYPE html>
+<head>
+    <title> Upload page </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-if(isset($_SESSION['id']) && isset($_SESSION['user_name'])){
+<body>
+    <nav class="topnav">
+        <a href="./imagefeed.php">Image feed</a>
+        <a class="active" href="./uploadpage.php">Upload image</a>
+        <a href="./userlist.php">User list</a>
+    </nav>
+    
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+
+
+<?php 
+
+#Jeg kan ikke få nav bar øverst??? 
+#session_start(); /*creates a session or resumes the current one based on a session identifier passed via a GET or POST request, or passed via a cookie.*/
+
+
+
+########################### FØLGENDE SIKRER AT MAN IKKE KAN TILGÅ DENNE SIDE MED MINDRE MAN ER LOGGET IND!!!!  ############
+/*if(isset($_SESSION['id']) && isset($_SESSION['user_name'])){
     ?>
 
     <!DOCTYPE html>
@@ -23,4 +48,6 @@ else {
     header("Location: index.php");
     exit();
 }
+?>*/
 ?>
+</html>
