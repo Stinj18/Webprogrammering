e37fbbd42a13756d4be55cd8b80e68c5df07ff03
Stinj18 <98690299+Stinj18@users.noreply.github.com>
2022-11-03 09:20:43 +0100

    Update userlist.php

--- Lektion 3/userlist.php
+++ Lektion 3/userlist.php
@@ -1,1 +1,47 @@
+<?php 
+session_start();
+include "config.php";
 
+//Fetch all users in table 
+$sql = "SELECT * FROM users;";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+<html>
+    <head>
+        <title> User list </title>
+        <link rel="stylesheet" type="text/css" href="imagefeedCSS.css">
+    </head>
+    
+    <body>
+
+        <header id="header">
+            <nav> 
+            <a href="./imagefeed.php">Image feed</a>
+            <a href="./uploadpage.php">Upload image</a>
+            <a href="./userlist.php">User list</a>
+            </nav>
+        </header>
+
+        <h1> Users: </h1>
+
+        <table class ="Userslist">
+            <tr>
+                <!-- <td> Usernames: </td> -->
+            </tr>
+            <tr>
+                <?php
+                    foreach ($result as $user) {
+                        echo "<tr>".
+                        "<td>" . $user["username"] . "</td>" .
+                        "</tr>";
+                    }
+                ?>
+            </tr>
+        </table>
+    </body>
+
+</html>
