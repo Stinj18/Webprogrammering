289101601e5795d2daddb3c7da743593470164ef
Stinj18 <98690299+Stinj18@users.noreply.github.com>
2022-11-03 10:32:00 +0100

    Update registrationpage.php

--- Lektion 3/registrationpage.php
+++ Lektion 3/registrationpage.php
@@ -3,16 +3,21 @@
         <title> Registration </title>
         <script src="registrationscript.js"></script>
 </head>
-<body>
-    <form method="post" onsubmit="return chechform()">
-        <h2>Registre here</h2>
-        Create username: <input type = "text" name="username" id="username"/>
-        <br>
-        Create password: <input type = "text" name="password" id="password"/>
-        <br>
-        <input type="submit" name="registre">
-    </form>
-
-</body>
+   <body>
+    <div class="container">
+      <form class="form" method="post" action=".Register.php">
+        <label for="name">Name</label> <br />
+        <input type="text" name="name" id="name" pattern="[a-zA-Z0-9]+" minlength="4" maxlength="10" placeholder="Name" required/> <br />
+        <label for="username">Username</label> <br />
+        <input type="text" name="username" id="username" pattern="[a-zA-Z0-9]+" minlength="4" maxlength="10" placeholder="Username" required/> <br />
+        <label for="password">Password</label> <br />
+        <input type="password" name="password" id="password" pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$" minlength="6" maxlength="16" placeholder="Password" required/> <br />
+        <label for="password2">Repeat password</label> <br />
+        <input type="password" name="password2" id="password2" pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$" minlength="6" maxlength="16" placeholder="Repeat password" required/> <br />
+        <button type="submit" name="button">Register</button>
+        <p>Already a user? Click <a href="index.php">HERE</a></p>
+      </form>
+    </div>
+  </body>
 
 </html>
