3dad8ab23e3bf9ccac13c2e5bdcf671489a50292
Stinj18 <98690299+Stinj18@users.noreply.github.com>
2022-11-08 08:14:53 +0100

    Update frontpage.php

--- Lektion 3/frontpage.php
+++ Lektion 3/frontpage.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
 include "config.php";
+include "index.php";
 
-if (isset($_POST['user_name']) && isset($_POST['password'])) {
-    $user_name = $_POST['user_name'];
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
     $password = $_POST['password'];
-    if (empty($user_name)) {
+    if (empty($username)) {
         echo("Location: index.php?error= Username is required");
     }
     elseif (empty($password)) {
@@ -13,29 +14,25 @@ if (isset($_POST['user_name']) && isset($_POST['password'])) {
     }
 }
 
-$sql = "SELECT * FROM user WHERE user_name = :username AND password = :password";
+/* $sql = "SELECT * FROM users WHERE username = :username AND password = :password";
 $stmt = $conn->prepare($sql);
-$stmt->bindParam(':username', $user_name);
+$stmt->bindParam(':username', $username);
 $stmt->bindParam(':password', $password);
 $stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC); */
 ?>
-<pre>
-    <?php
-print_r($result);
-?>
-</pre>
+
 
 <?php
 //OBS i denne del skal vi være obs på om det er user eller users og user_name eller username
 if(ISSET($_POST['login_b'])){
-    if($_POST['user_name'] != "" || $_POST['password'] != ""){
-        $user_name = $_POST['user_name'];
+    if($_POST['username'] != "" || $_POST['password'] != ""){
+        $username = $_POST['username'];
         $password = $_POST['password'];
-        $sql = "SELECT * FROM user WHERE user_name = :user_name AND password= :password";
+        $sql = "SELECT * FROM users WHERE username = :username AND password= :password";
 
         $query = $conn->prepare($sql);
-        $query->bindParam(':user_name', $user_name);
+        $query->bindParam(':username', $username);
         $query->bindParam(':password', $password);
         
         //En anden måde at logge in på
@@ -45,20 +42,22 @@ if(ISSET($_POST['login_b'])){
         $query -> execute();
         $row = $query->rowCount();
         $fetch = $query->fetch();
+        print($username);
+
         if($row > 0) {
-            $_SESSION['user_name'] = $fetch['password'];
+            $_SESSION['username'] = $fetch['password'];
             header("location: uploadpage.php");
-        } else{
-            echo "
+        }  else{
+             echo "
             <script>alert('Invalid username or password')</script>
-            <script>window.location = 'index.php'</script>
-            ";
+            <script>window.location = 'frontpage.php'</script>
+            "; 
         }
     }
     else{
         echo "
         <script>alert('Please complete the required field!')</script>
-        <script>window.location = 'index.php'</script>
+        <script>window.location = 'frontpage.php'</script>
         ";
     }
 }
