2905cc797f28ae87d0eb64f680e2eb2ef44687b7
Stinj18 <98690299+Stinj18@users.noreply.github.com>
2022-12-01 17:19:50 +0100

    Delete mvc-main directory

--- mvc-main/app/views/partials/head.php
+++
@@ -1,9 +0,0 @@
-<title>Falster og Malles fede billedbog</title>
-
-<link media="all" rel="stylesheet" href="/public/css/style.css">
-
-<?php
- /**
-  * This file contains everything that goes into the html head section
-  * it is loaded by the template 
-  */
--- mvc-main/public/js/scripts.js
+++
@@ -1,6 +0,0 @@
-/**
- * This file simply proves that we can import our own javascript from views.
- * This file is included from app/views/partials/footer
- * 
- */
-console.log("My Script");
