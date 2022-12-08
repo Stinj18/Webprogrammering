2f60c4b3bd283d89457e2363afdc72d5de6ffec2
Stinj18 <98690299+Stinj18@users.noreply.github.com>
2022-12-08 17:16:28 +0100

    Delete mvc-main directory

--- mvc-main/app/views/partials/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    Falster og Malles fede billedebog | Syddansk Universitet
-</footer>
-<?php 
- /**
-  * The footer both allows the site to get a footer and for a place to put your JS imports
-  */
-?>
-<script src="/public/js/scripts.js"></script>
--- mvc-main/app/views/partials/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-    Falster og Malles fede billedbog
-</header>
-<?php
-
-/** 
- * See this as a letterhead - something that is visible on the top of each page
- * Do not confuse this with the HTML HEAD
- */
